2a9e035e728e1b7e1f17171578f8fde51f21bd6b
Lukman Mohammed <olalekanlukmanmohammed@yahoo.com>
2021-10-27 07:44:33 -0700

    added header

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,5 +24,9 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
+
 
 #endif
